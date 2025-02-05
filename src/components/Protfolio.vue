af145f5fa58c65ce1e84fdd6bc5a156852e1c0c6
Ali Fasihi <im.alifasihi@gmail.com>
2025-02-05 12:51:11 +0330

    Adding minor changes

--- src/components/Protfolio.vue
+++ src/components/Protfolio.vue
@@ -12,26 +12,26 @@ import TitleCard from "./Titledisplay.vue";
       <h3>آخرین پروژه ها</h3>
     </div>
     <div class="latest-project">
-      <div class="latest-projects">
-        <ImageCard src="/src/assets/svgs/i5.svg" />
+      <div class="latest-projects btns">
+        <ImageCard src="/src/assets/images/i5.png" />
         <div class="project-description">
-          <TitleCard text="Simple-Login-Form-Vue-JS" />
-          <a href="https://github.com/alifasihi/Simple-Login-Form-Vue-JS-1" class="btns btn btn-primary" id="btn-needfood" target="_blank">مشاهده گیت هاب</a>
+          <TitleCard text="توسعه فرم ورود ساده با Vue js" />
+          <a href="https://github.com/alifasihi/Simple-Login-Form-Vue-JS-1" class="btns btn btn-primary" target="_blank">مشاهده گیت هاب</a>
         </div>
       </div>
-      <div class="latest-projects">
+      <div class="latest-projects btns">
         <ImageCard src="/src/assets/images/i3.png" />
 
         <div class="project-description">
-          <TitleCard text="توسعه وبسایت خیابان کافه" />
-          <a href="#" class="btns btn btn-primary" id="btn-cafestreet">مشاهده در گیت هاب</a>
+          <TitleCard text="توسعه وبسایت شخصی با Vue js" />
+          <a href="#" class="btns btn btn-primary">مشاهده در گیت هاب</a>
         </div>
       </div>
-      <div class="latest-projects">
+      <div class="latest-projects btns">
         <ImageCard src="/src/assets/images/i4.png" />
         <div class="project-description">
-          <TitleCard text="توسعه وبسایت مسافربری" />
-          <a href="#" class="btns btn btn-primary" id="btn-travelapp">مشاهده در گیت هاب</a>
+          <TitleCard text="توسعه وبسایت پوشاک و مد با html و css ساده" />
+          <a href="https://github.com/alifasihi/Fashion-website-with-HTML-CSS" class="btns btn btn-primary">مشاهده در گیت هاب</a>
         </div>
       </div>
     </div>
